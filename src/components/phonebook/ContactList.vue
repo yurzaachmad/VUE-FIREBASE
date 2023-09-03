378af2347236b090a7cebfced27ee27007126868
yurza <yurzaachmad@gmail.com>
2023-09-03 18:32:27 +0700

    update scrolling and searching

--- src/components/phonebook/ContactList.vue
+++ src/components/phonebook/ContactList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch, onMounted, onUnmounted, computed } from 'vue'
 import ContactItem from './ContactItem.vue'
 import { useContactsStore } from '@/stores/contacts'
 
@@ -13,59 +13,56 @@ const props = defineProps({
 
 const ITEMS_PER_PAGE = 10
 const currentPage = ref(1)
-const currentItems = ref([])
+const isLoading = ref(false)
 
 const store = useContactsStore()
 const contacts = ref([])
-const currentSort = ref(props.currentSort)
+
+const filteredContacts = computed(() => {
+  return contacts.value.filter((contact) => {
+    return props.searchParam.some((param) => {
+      return contact[param].toLowerCase().includes(props.query.toLowerCase())
+    })
+  })
+})
+
+const sortedContacts = computed(() => {
+  return [...filteredContacts.value].sort((a, b) => {
+    if (props.currentSort === 'up') {
+      return a.name.localeCompare(b.name)
+    } else {
+      return b.name.localeCompare(a.name)
+    }
+  })
+})
+
+const currentItems = computed(() => {
+  const startIndex = (currentPage.value - 1) * ITEMS_PER_PAGE
+  const endIndex = startIndex + ITEMS_PER_PAGE
+  return sortedContacts.value.slice(0, endIndex)
+})
+
+const handleScroll = () => {
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement
+  if (scrollTop + clientHeight >= scrollHeight && !isLoading.value) {
+    isLoading.value = true
+    currentPage.value++
+    setTimeout(() => {
+      isLoading.value = false
+    }, 1000) // Add a delay to avoid loading more data too quickly
+  }
+}
 
 onMounted(async () => {
   await store.loadContact()
   contacts.value = store.contacts
-
-  // Perform initial sorting
-  sortContacts()
-})
-
-// Watch for changes in currentPage and currentSort, and update currentItems accordingly
-watch([currentPage, () => props.currentSort], () => {
-  sortContacts()
-})
-
-// Function to handle the scroll event and trigger pagination
-const handleScroll = () => {
-  const { scrollTop, clientHeight, scrollHeight } = document.documentElement
-  if (scrollTop + clientHeight >= scrollHeight) {
-    currentPage.value++
-  }
-}
-
-// Attach the scroll event listener when the component mounts
-onMounted(() => {
   window.addEventListener('scroll', handleScroll)
-  // Remove the scroll event listener when the component unmounts
-  return () => window.removeEventListener('scroll', handleScroll)
 })
 
-// Sort contacts based on the currentSort prop
-const sortContacts = () => {
-  const startIndex = (currentPage.value - 1) * ITEMS_PER_PAGE
-  const endIndex = startIndex + ITEMS_PER_PAGE
-  const sortedContacts = [...contacts.value].sort(props.sortTypes[props.currentSort].fn)
-
-  if (currentSort.value !== props.currentSort) {
-    // If sorting changes, reset to page 1 and clear currentItems
-    currentPage.value = 1
-    currentSort.value = props.currentSort
-    currentItems.value = []
-  } else if (currentPage.value === 1) {
-    // Clear currentItems when sorting is the same but on page 1
-    currentItems.value = []
-  }
-
-  // Append the sorted data to the currentItems array without clearing previous data
-  currentItems.value.push(...sortedContacts.slice(startIndex, endIndex))
-}
+// Remove the scroll event listener when the component unmounts
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
